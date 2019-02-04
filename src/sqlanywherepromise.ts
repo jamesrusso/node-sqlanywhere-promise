64d93967ccb1e08c8458ef2b6bcf504643121d5e
James Russo <jr@halo3.net>
2019-02-03 19:59:41 -0500

    cleanup if/then and resolve usage.

--- src/sqlanywherepromise.ts
+++ src/sqlanywherepromise.ts
@@ -3,41 +3,41 @@ import { ConnectionParameters, createConnection, Statement } from "sqlanywhere";
 export interface SQLAnywhereConnectionParameters extends ConnectionParameters {
 }
 
-export class SQLAnywhereStatement { 
-  
-  constructor(private _statement: Statement) { 
+export class SQLAnywhereStatement {
+
+  constructor(private _statement: Statement) {
   }
 
-  public exec(args : any[]) : Promise<any[] | Error> { 
-    return new Promise((resolve, reject) => { 
+  public exec(args: any[]): Promise<any[] | Error> {
+    return new Promise((resolve, reject) => {
       this._statement.exec(args, (err, rows) => {
-      if (err) { 
-         return reject(err);
-      }
-      resolve(rows);
-     })
+        if (err) {
+          return reject(err);
+        }
+        resolve(rows);
+      })
     })
-  } 
+  }
 }
 
 export class SQLAnywhereConnection {
   private _conn = createConnection();
 
-  public connect(params : SQLAnywhereConnectionParameters) : Promise<Error | undefined> { 
-    return new Promise((resolve, reject) => { 
-      return this._conn.connect(params, (err) => { 
-          if (err) { 
-            return reject(err);
-          }
-          resolve();
+  public connect(params: SQLAnywhereConnectionParameters): Promise<Error | undefined> {
+    return new Promise((resolve, reject) => {
+      return this._conn.connect(params, (err) => {
+        if (err) {
+          return reject(err);
+        }
+        resolve();
       })
     });
   }
 
-  public prepare(sql: string) : Promise<SQLAnywhereStatement> { 
-    return new Promise((resolve, reject) => { 
-      this._conn.prepare(sql, (err, stmt) => { 
-        if (err) { 
+  public prepare(sql: string): Promise<SQLAnywhereStatement> {
+    return new Promise((resolve, reject) => {
+      this._conn.prepare(sql, (err, stmt) => {
+        if (err) {
           return reject(err);
         }
         resolve(new SQLAnywhereStatement(stmt));
@@ -49,45 +49,42 @@ export class SQLAnywhereConnection {
     return new Promise((resolve, reject) => {
       this._conn.exec(sql, params || [], (err: Error | undefined, rows: any) => {
         if (err) {
-          reject(err);
+          return reject(err);
         }
         resolve(rows);
       })
     });
   }
 
-  public commit() : Promise<Error | void> { 
-    return new Promise((resolve, reject) => { 
-      this._conn.commit((err) => { 
-        if (err) { 
-          reject(err);
-        } else { 
-          resolve();
+  public commit(): Promise<Error | void> {
+    return new Promise((resolve, reject) => {
+      this._conn.commit((err) => {
+        if (err) {
+          return reject(err);
         }
+        resolve();
       })
     });
   }
 
-  public rollback() : Promise<Error | void> { 
-    return new Promise((resolve, reject) => { 
-      this._conn.rollback((err) => { 
-        if (err) { 
+  public rollback(): Promise<Error | void> {
+    return new Promise((resolve, reject) => {
+      this._conn.rollback((err) => {
+        if (err) {
           reject(err);
-        } else { 
-          resolve();
         }
+        resolve();
       })
     });
   }
 
-  public close() : Promise<Error | void> { 
-    return new Promise((resolve, reject) => { 
-      this._conn.close((err) => { 
-        if (err) { 
+  public close(): Promise<Error | void> {
+    return new Promise((resolve, reject) => {
+      this._conn.close((err) => {
+        if (err) {
           reject(err);
-        } else { 
-          resolve();
         }
+        resolve();
       })
     })
   }
